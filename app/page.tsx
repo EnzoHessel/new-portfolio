7625ac68ff6635baf737640ad4c6baa4831449b5
Enzo Hessel <enzo.carmo@salg.com.br>
2024-07-26 15:46:44 -0300

    fix: background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Header from "@/components/Header";
 import HeroContent from "@/components/HeroContent";
+import Motivational from "@/components/Motivational";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full max-w-[1024px] flex-col items-center mx-auto p-4 sm:p-6 md:p-8 lg:p-10 bg-[#23262B]">
+    <main className="flex min-h-screen w-full max-w-[1024px] flex-col items-center mx-auto p-4 sm:p-6 md:p-8 lg:p-10 bg-[#282C33]">
       <Header/>
       <div className="pt-8">
         <HeroContent/>
+      </div>
+      <div className="pt-28">
+        <Motivational />
       </div>
     </main>
   );
